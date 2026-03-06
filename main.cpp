a1c7eef83e68c81cb92393cccb1c714dc683bd13
AZONHOUMON Houénoumè Horacia Gloriéta <azonhoumonhoracia@gmail.com>
2026-03-06 16:19:57 +0000

    Delete main.cpp

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, ARIA! My first C++ program." << std::endl;
-    std::cout << "I am learning embedded systems." << std::endl;
-    
-    return 0;
-}
