fb04442370920a0047d881aa9f524d642d10d68a
Lady_Horacia <azonhoumonhoracia@gmail.com>
2026-03-06 11:33:39 +0000

    feat: first C++ program - hello world and iostream basics

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, ARIA! My first C++ program." << std::endl;
+    std::cout << "I am learning embedded systems." << std::endl;
+    
+    return 0;
+}
